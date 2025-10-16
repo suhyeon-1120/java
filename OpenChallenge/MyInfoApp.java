f74fcfbb6cfbf00752d8c706c9f32e1a4eef14c0
suhyeon <suhyeon3626@gmail.com>
2025-10-16 17:38:31 +0900

    Add files via upload

---
+++ OpenChallenge/MyInfoApp.java
@@ -0,0 +1,15 @@
+package Openchallenge1;
+
+public class MyInfoApp {
+	public static void main(String[] args) {
+		
+		String name = "Kitae Hwang";
+		int age= 20;
+		String major= "Department of Computer Engineering";
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(major);
+		
+	}
+}
