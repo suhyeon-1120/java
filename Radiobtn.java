c3fbda87470abc9aa97c6fbe9c50144b11394d3b
suhyeon <suhyeon3626@gmail.com>
2025-11-13 14:08:08 +0900

    Add files via upload

---
+++ Radiobtn.java
@@ -0,0 +1,30 @@
+package q;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Radiobtn extends JFrame {
+	public Radiobtn() {
+		setTitle("라디오버튼 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		ButtonGroup g = new ButtonGroup();
+		
+		JRadioButton apple = new JRadioButton("사과");
+		JRadioButton pear = new JRadioButton("배", true);
+		JRadioButton cherry = new JRadioButton("체리");
+
+		g.add(apple);
+		g.add(pear);
+		g.add(cherry);
+		
+		
+		c.add(apple); c.add(pear); c.add(cherry);
+		setSize(250,150);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new Radiobtn();
+	}
+}
